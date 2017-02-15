758c608dce0d3e11287f09248a877e5da362e3d1
Guillaume Gautreau <guillaume@ghusse.com>
2017-02-15 21:44:12 +0100

    Runs the example with callbacks

--- index.js
+++ index.js
@@ -6,12 +6,16 @@
 //   .each(console.log);
 
 
-const promiseLoad = require('./paginationPromise');
+// const promiseLoad = require('./paginationPromise');
 
-const before = Date.now();
-promiseLoad()
-  .then(console.log)
-  .then(() => {
-    const after = Date.now();
-    console.log(`${after - before}ms`);
-  });
+// const before = Date.now();
+// promiseLoad()
+//   .then(console.log)
+//   .then(() => {
+//     const after = Date.now();
+//     console.log(`${after - before}ms`);
+//   });
+
+const promiseCallbackLoad = require('./paginationPromiseWithCallbacks')
+
+promiseCallbackLoad((error, result) => console.log(result));
